601173ecc8a26950669394fc81caea8f1f4091b4
jacek <202203150323@zjut.edu.cn>
2024-02-06 21:10:17 +0800

    remove utils.rs

--- src/api/utils.rs
+++
@@ -1,9 +0,0 @@
-use std::io::Cursor;
-use axum::body::Bytes;
-use image::{io::Reader as ImageReader, ColorType};
-
-// 压缩图片
-fn compressImage(image_data: Vec<u8>) -> String {
-    let image_data = ImageReader::new(Cursor::new(image_data)).with_guessed_format().expect("Cursor io never fails").decode().expect("decode image failed");
-    todo!();
-}
